d2c0bd32862f2a21c289364e380681a2925b353d
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-18 16:10:04 +0200

    args, stdout.write, dynamic

--- firstAndExample.dart
+++ firstAndExample.dart
@@ -1,52 +1,54 @@
 // import 'dart:ffi';
 import 'dart:io';
 
-void main () {
+void main(List<String> nameArg) {
+  // print(nameArg);
+
   final String greet = 'Hello\nThis is my first dart programm';
-  String name;
+  String name = nameArg[0];
+  dynamic decision;
 
   double result = 0.0;
 
-  List<int> numbers = [3, 5, 7 , 13];
+  /* 
+    List<int> numbers = [3, 5, 7 , 13];
+    Map<String, int> divMap = {'Erster Key': 1, 'Zweiter Kex': 2, 'Dritter Kex': 3, 'Vierter Key': 4}; 
+  */
 
-  var decision;
-  
   print('\n\t-->Start<--\n');
   print('$greet\n');
 
-  print('Insert name: ');
-  name = stdin.readLineSync()!;    
-  
+  /* 
+    stdout.write('Insert name: ');
+    print(name);
+    name = stdin.readLineSync()!; 
+  */
+
   print('\n--------------------------------------------------');
   print('\nHello $name\n');
 
-  
-  print('\nCalculator (calc*) or perosnal information (pi)?');
-  print('Zahlenbereich -9.223.372.036.854.775.808 bis 9.223.372.036.854.775.807.');
+  print('\nCalculator (calc) or perosnal information (pi)?');
+
   decision = stdin.readLineSync();
+  decision = decision.toLowerCase();
+  print('\nYou have chosen: $decision\n');
 
-  if(decision == 'calc') {
-
+  if (decision == 'calc' || decision == 'calculator') {
     result = calc();
     String numberFormat = result.toStringAsFixed(4);
     print('\nResult: $numberFormat');
-
-  } else if(decision == 'pi' && name == 'Fabian') {
+  } else if (decision == 'pi' && name == 'Fabian') {
     personalInfo();
-
-  } else if(decision == 'pi' && name != 'Fabian') {
+  } else if (decision == 'pi' && name != 'Fabian') {
     decision = 'personal information';
     print('\nYou are not Fabian, you are $name !!!');
     print('You are not allowed to see the $decision !!!');
-
   } else {
     print('\nYou have to decide between calculator or personal information');
-
   }
-  
+
   print('\nWas für ein Hohn');
   print('\n\t-->Ende<--\n');
-
 }
 // ############################################################################################
 // Claculator Function Section
@@ -54,22 +56,20 @@ void main () {
 
 // double calc(double zahl, double zahl2)
 double calc() {
+  double functionResult = 0.0;
+  stdout.write('Wähle die Operation (+, -, *, /): ');
+  var choice = stdin.readLineSync();
 
-double functionResult = 0.0;
-print('Wähle die Operation');
-var choice = stdin.readLineSync();
+  stdout.write('Insert number #1: ');
+  double zahl = double.parse(stdin.readLineSync()!);
 
-print('Insert number #1: ');
-double zahl = double.parse(stdin.readLineSync()!);
+  stdout.write('Insert number #2: ');
+  double zahl2 = double.parse(stdin.readLineSync()!);
 
-print('Insert number #2: ');
-double zahl2 = double.parse(stdin.readLineSync()!);
-
-switch (choice) {
+  switch (choice) {
     case '+':
       functionResult = zahl + zahl2;
-      break;
-    case '-': 
+    case '-':
       functionResult = zahl - zahl2;
       break;
     case '*':
@@ -93,11 +93,17 @@ switch (choice) {
 // ############################################################################################
 
 personalInfo() {
-  final Map<String, dynamic> personalInformation = {'\nName' : 'Strottmann' , '\nFirst Name' : 'Fabian' , '\nBirth date' : '09.07.1988' , 
-  '\nAddress' : 'Schilfkamp 20' , '\nPostal code' : 30851 , '\nLocation' : 'Langehagen'};
+  final Map<String, dynamic> personalInformation = {
+    '\nName': 'Strottmann',
+    '\nFirst Name': 'Fabian',
+    '\nBirth date': '09.07.1988',
+    '\nAddress': 'Schilfkamp 20',
+    '\nPostal code': 30851,
+    '\nLocation': 'Langehagen',
+  };
 
   print('\n\nMapgroesse');
   print(personalInformation.length);
 
   print('\n\nPersonal information\n $personalInformation');
- }
+}
