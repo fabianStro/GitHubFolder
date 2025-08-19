e111c42d80e7e6a0687f487c2b3e37e3da6aab3f
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-19 14:58:36 +0200

    Aufgabe 1 Variablen und Konstanten

--- Aufgaben/variablenUndKondtantenAufg1.dart
+++ Aufgaben/variablenUndKondtantenAufg1.dart
@@ -1,12 +1,20 @@
 void main() {
-  String gdStr = 'Guten', dayStr = 'Tag', worldStr = 'Welt', timeStr = 'Zeit', 
-          dString = '!', c = 'Schoene', morning = 'Morgen', s = 'Heute', spa= ' ';
+  String gdStr = 'Guten',
+      dayStr = 'Tag',
+      worldStr = 'Welt',
+      timeStr = 'Zeit',
+      dString = '!',
+      c = 'Schoene',
+      morning = 'Morgen',
+      s = 'Heute',
+      spa = ' ';
   int d = 20, seven = 7, twelve = 12, a = 24;
 
-print(gdStr + spa + dayStr + spa + worldStr + dString);
-print(a + seven);
-print(c + spa + timeStr + dString);
-print(s + spa + morning + dString);
-print(twelve * 14);
+  print('Variablen und Konstanten Aufgabe 1\n');
 
+  print(gdStr + spa + dayStr + spa + worldStr + dString);
+  print(a + seven);
+  print(c + spa + timeStr + dString);
+  print(s + spa + morning + dString);
+  print(twelve * 14);
 }
