4b25654cfda7771ae850d63af921456d8ed2b667
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-22 11:03:11 +0200

    Schleifen 1 Aufgabe 1

---
+++ Aufgaben/schleifen1Aufg1.dart
@@ -0,0 +1,16 @@
+void main() {
+  List<int> points = [4, 5, 4, 2, 6, 6, 3];
+  List<String> names = [
+    'Julietta',
+    'Benjamino',
+    'Hans-Guenther',
+    'Evalinea',
+    'Fiona',
+    'Gregory',
+    'Leonhart',
+  ];
+
+  for (String name in names) {
+    print('Name: $name');
+  }
+}
