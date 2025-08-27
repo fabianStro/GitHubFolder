852be5a8ecc9095c7ee72cc404de62723cb611f0
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-27 11:10:43 +0200

    funktonen 1 Aufgabe 3

---
+++ Aufgaben/funktionen1_Aufg3.dart
@@ -0,0 +1,20 @@
+void main(List<String> args) {
+  /* List<String> jd = ['Johnny', 'Depp'];
+  List<String> tc = ['Tom', 'Cruise']; */
+  String vName1 = 'Johnny', vName2 = 'Tom', name1 = 'Depp', name2 = 'Cruise';
+  reverseInitials(vName1, vName2, name1, name2);
+}
+
+reverseInitials(
+  String vorname1,
+  String vorname2,
+  String nname1,
+  String nname2,
+) {
+  print(
+    'First Initials: ${vorname1[5].toUpperCase()}.${nname1[3].toUpperCase()}.',
+  );
+  print(
+    'First Initials: ${vorname2[2].toUpperCase()}.${nname2[4].toUpperCase()}.',
+  );
+}
