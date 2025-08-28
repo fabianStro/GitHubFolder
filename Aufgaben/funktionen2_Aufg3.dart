b314cebc37c2b8ad7883902f0d165739d085e73c
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-08-28 11:58:18 +0200

    Funktionen 2 Aufgabe 2 und Aufgabe 3

--- Aufgaben/funktionen2_Aufg3.dart
+++ Aufgaben/funktionen2_Aufg3.dart
@@ -6,11 +6,39 @@ void main(List<String> args) {
   String text3 =
       'Das Buch handelt weitgehend von Hobbits, und aus seinen Seiten kann ein Leser viel ueber ihren Charakter und ein wenig ueber ihre Geschichte erfahren. Weitere Einzelheiten sind auch in der Auswahl aus dem Roten Buch der Westmark zu finden, die unter dem Titel Der Hobbit bereits veroeffentlicht wurde. Jene Darstellung stammt aus den ersten Kapiteln des Roten Buches, die Bilbo selbst, der erste Hobbit, der in der ganzen Welt beruehmt wurde, verfasst und die er Hin und wieder zurueck genannt hat, er erzaehlt darin von seiner Fahrt in den Osten und seiner Rueckkehr: ein Abenteuer, durch das spaeter alle Hobbits in die grossen Ereignisse jenes Zeitalters, von denen hier berichtet wird, hineingezogen wurden. Viele Leser moegen indes gleich zu Beginn mehr ueber dieses bemerkenswerte Volk wissen wollen, waehrend manche vielleicht das erste Buch nicht besitzen. Fuer sie seien hier einige der wichtigeren Punkte aus der Hobbitkunde zusammengestellt und das erste Abenteuer kurz wiedergegeben.';
 
-  print('${countCharactersInText(text1)} Zeichen');
-  print('${countCharactersInText(text2)} Zeichen');
-  print('${countCharactersInText(text3)} Zeichen');
+  print('Gesamt Vokale Text #1: ${countVowels(text1)}\n');
+  print('Gesamt Vokale Text #2: ${countVowels(text2)}\n');
+  print('Gesamt Vokale Text #3: ${countVowels(text3)}\n');
 }
 
-int countCharactersInText(String text) {
-  return text.length;
+int countVowels(String text) {
+  int countA = 0, countE = 0, countI = 0, countO = 0, countU = 0;
+
+  for (var i = 0; i < text.length - 1; i++) {
+    switch (text[i]) {
+      case 'a':
+        countA++;
+        break;
+      case 'e':
+        countE++;
+        break;
+      case 'i':
+        countI++;
+        break;
+      case 'o':
+        countO++;
+        break;
+      case 'u':
+        countU++;
+        break;
+      default:
+    }
+  }
+  print('A: $countA mal');
+  print('E: $countE mal');
+  print('I: $countI mal');
+  print('O: $countO mal');
+  print('U: $countU mal');
+
+  return countA + countE + countI + countO + countU;
 }
