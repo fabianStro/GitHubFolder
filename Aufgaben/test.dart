f057e272ae6dc4b3fc866096dc188d549f601dac
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-09-03 14:47:30 +0200

    Test

--- Aufgaben/test.dart
+++ Aufgaben/test.dart
@@ -8,4 +8,21 @@ void menu() {
 
 void showClass() {
   print('');
+  /*   void main(List<String> args) {
+  Button b = Button('In den Warenkorb', width: 96, height: 32, radius: 4, fontSize: 14, padding: 8);
+}
+
+class Button {
+  String text;
+  int width, height, radius, fontSize, padding;
+
+  Button(
+    this.text, {
+    required this.width,
+    required this.height,
+    required this.radius,
+    required this.fontSize,
+    required this.padding,
+  });
+ }*/
 }
