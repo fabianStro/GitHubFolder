c62c4cbce14b7d666931c3891e524166e7ad83cd
Fabian Strottmann <fabian.strottmann@gmail.com>
2025-09-08 08:55:41 +0200

    Databse Repository Aufg 2 und 3

--- Aufgaben/databaseRepository_Aufg2.dart
+++ Aufgaben/databaseRepository_Aufg2.dart
@@ -1,13 +1,55 @@
 void main(List<String> args) {
   // main Klasse
-  DataRepository dR = MockDatabaseRepository([]);
-  dR.sendMaessage('Erste Maessage');
-  dR.sendMaessage('Zweite Message');
-  print(dR.getMessages());
+  DataRepository dR1 = MockDatabaseRepository([]);
+  DataRepository dR2 = Broadcast([], ' ', ' ', 0.0);
 
-  dR.delete(0);
+  dR1.add('Erste Maessage');
+  dR1.add('Zweite Message');
+  print('\nMessage List');
+  print(dR1.show());
+
+  dR2.add('Highschool DxD / Haisukūru Dī Dī');
+  dR2.add('Dragonball / Doragonbōru');
+  dR2.add('Naruto Shippūden');
+  dR2.add('The Eminence in Shadow / Kage no Jitsuryokusha ni Naritakute');
+  dR2.add('The Hero Is Overpowered but Overly Cautious / Kono Yūsha ga Ore Tsuē Kuse ni Shinchō Sugiru');
+  print('\nBroadcast List');
+  print(dR2.show());
+
+  /* dR1.delete(0);
   print('Removed');
-  print(dR.getMessages());
+  print(dR1.show()); */
+}
+
+class Broadcast implements DataRepository {
+  List<String> broadcastList;
+  String title, description;
+  double duration;
+
+  Broadcast(this.broadcastList, this.title, this.description, this.duration);
+
+  @override
+  void add(String title) {
+    broadcastList.add('\n' + title);
+  }
+
+  @override // getMessage/ read
+  List<String> show() {
+    return broadcastList;
+  }
+
+  @override
+  void update() {}
+
+  @override // delete
+  void delete(int index) {
+    broadcastList.removeAt(index);
+    if (broadcastList.isNotEmpty) {
+      broadcastList.removeAt(index);
+    } else {
+      print('Liste leer');
+    }
+  }
 }
 
 //####################################################
@@ -18,13 +60,13 @@ class MockDatabaseRepository implements DataRepository {
   MockDatabaseRepository(this.messages);
 
   // Overrides ##################
-  @override // adden/ create
-  void sendMaessage(String message) {
-    messages.add(message);
+  @override // adden
+  void add(String message) {
+    messages.add('\n' + message);
   }
 
-  @override // getMessage/ read
-  List<String> getMessages() {
+  @override // show
+  List<String> show() {
     return messages;
   }
 
@@ -33,14 +75,18 @@ class MockDatabaseRepository implements DataRepository {
 
   @override // delete
   void delete(int index) {
-    messages.removeAt(index);
+    if (messages.isNotEmpty) {
+      messages.removeAt(index);
+    } else {
+      print('Liste leer');
+    }
   }
 }
 
 //####################################################
 abstract class DataRepository {
-  void sendMaessage(String message);
-  List<String> getMessages();
+  void add(String message);
+  List<String> show();
   void update();
   void delete(int index);
 }
